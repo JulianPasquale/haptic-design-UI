227cfa6da4de3995522b8a6c1feef754b4007d1d
Julian Pasquale <julianagustin78@gmail.com>
2020-11-22 19:17:44 -0300

    Initial setup for React app with TypeScript

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module'
+	},
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended'
+	],
+	rules: {
+		'no-console': 'warn'
+	}
+};
