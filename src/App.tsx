80416f6abe5672aee648dcbbb5899047ed427ec1
Julian Pasquale <julianagustin78@gmail.com>
2020-11-22 20:05:14 -0300

    Fix eslint conflict rules and change TypeScript version to fix create-react-app issue: https://github.com/facebook/create-react-app/issues/10110

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
-      </p>
+			</p>
       <a
         className="App-link"
         href="https://reactjs.org"
@@ -16,7 +16,7 @@ const App = () => (
         rel="noopener noreferrer"
       >
         Learn React
-      </a>
+			</a>
     </header>
   </div>
 );
