0b958b09018573af8bdfaae8be9009c68b6672bb
Julian Pasquale <julianagustin78@gmail.com>
2020-12-05 16:32:28 -0300

    Add new dot button

--- src/pages/EditVibration.tsx
+++ src/pages/EditVibration.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Chart from '../components/Chart';
 
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
 const EditVibration: React.FC = (): React.ReactElement => (
-  <div style={{ width: '90%', height: '600px' }}>
-    <Chart />
-  </div>
+  <>
+    <div style={{ width: '90%', height: '600px' }}>
+      <Chart />
+    </div>
+
+    <Fab variant='extended' color='primary' aria-label='add'>
+      <AddIcon />
+      Agregar nuevo punto
+    </Fab>
+  </>
 );
 
 export default EditVibration;
