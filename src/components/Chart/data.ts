65d9ad904598e5f47c73bda0da05702429d93446
Julian Pasquale <julianagustin78@gmail.com>
2020-12-05 19:34:13 -0300

    Change data value to draw a straight line

--- src/components/Chart/data.ts
+++ src/components/Chart/data.ts
@@ -1,17 +1,12 @@
 import { Point } from './index.d';
 
 const data: Point[] = [
-  { name: 0, value: 4.11, },
-  { name: 1, value: 2.39, },
-  { name: 2, value: 1.37, },
-  { name: 3, value: 1.16, },
-  { name: 4, value: 2.29, },
-  { name: 5, value: 3, },
-  { name: 6, value: 0.53, },
-  { name: 7, value: 2.52, },
-  { name: 8, value: 1.79, },
-  { name: 9, value: 2.94, },
-  { name: 10, value: 4.3, },
+  { name: 0, value: 100, },
+  { name: 1, value: 100, },
+  { name: 2, value: 100, },
+  { name: 3, value: 100, },
+  { name: 4, value: 100, },
+  { name: 5, value: 100, },
 ];
 
 export default data;
