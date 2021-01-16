d23edf9f4bf2c328e0a63560e2947c56a49fc446
Julian Pasquale <julianagustin78@gmail.com>
2021-01-16 14:11:06 -0300

    Add react-router-dom and initial home design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import EditVibration from './modules/EditVibration';
 
 // material-ui
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 
+// react-router-dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+// modules
+import EditVibration from './modules/EditVibration';
+import Home from './modules/Home';
+
 const App: React.FC = (): React.ReactElement => (
   <>
     <CssBaseline />
     <Container>
-      <EditVibration />
+      <Router>
+        <Switch>
+          <Route path='/edit/:vibrationId?'>
+            <EditVibration />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </Container>
   </>
 );
---
+++ src/components/Layout/AppBar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+// material-ui
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import styled from 'styled-components';
+
+import { drawerWidth } from './Constants';
+
+const CustomAppBar = styled(AppBar)`
+  width: calc(100% - ${drawerWidth}px);
+  marginLeft: ${drawerWidth};
+`;
+
+export default (): React.ReactElement => (
+  <CustomAppBar position='fixed'>
+    <Toolbar>
+      <Typography variant='h6' noWrap>
+        Haptic Design
+      </Typography>
+    </Toolbar>
+  </CustomAppBar>
+)
